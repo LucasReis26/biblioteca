f25744a8b6a3c04df3e488b646bcb627a781f510
Lucas Reis <luctreis@gmail.com>
2025-02-11 15:55:44 -0300

    Minha biblioteca e algoritmos

---
+++ algs/verC.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	char dir[255];
+	char sFile[255];
+	char cmd[255];
+	FILE *file;
+
+	fgets(dir, 255, stdin);
+	dir[strlen(dir) - 1] = '\0';
+
+	sprintf(cmd,"ls %s | grep '\\.c$'", dir);
+	file = popen(cmd,"r");
+	if(file == NULL){
+		perror("Failed to run program");
+		return 1;
+	}
+
+	fgets(sFile, 255, file);
+	sFile[strlen(sFile) - 1] = '\0';
+	printf("%s nome do arquivo\n",sFile);
+
+	sprintf(cmd,"gcc %s/%s -o %s/exec",dir,sFile,dir);
+	system(cmd);
+
+	sprintf(cmd,"./%s/exec < %s/pubs/pub.in > %s/program.out",dir,dir,dir);
+	system(cmd);
+
+	sprintf(cmd,"vimdiff %s/program.out %s/pubs/pub.out",dir,dir);
+	system(cmd);
+
+	return 0;
+}
---
+++ algs/verJ.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void getClass(char *sFile){
+	for(int i = 0; i < strlen(sFile); i++){
+		if(sFile[i] == '.')
+			sFile[i] = '\0';
+	}
+	return;
+}
+
+int main(){
+	FILE *file;
+	char sFile[255];
+	char dir[255];
+	char cmd[255];
+
+	fgets(dir, 255, stdin);
+	dir[strlen(dir) - 1] = '\0';
+
+	sprintf(cmd,"ls %s | grep '\\.java$'", dir);
+
+	file = popen(cmd,"r");
+	if (file == NULL){
+		perror("Failed to run command");
+		return 1;
+	}
+
+	fgets(sFile, 255, file);
+	sFile[strlen(sFile) - 1] = '\0';
+
+	sprintf(cmd,"javac %s/%s", dir, sFile);
+	system(cmd);
+
+	getClass(sFile);
+
+	sprintf(cmd,"java %s.%s < %s/pubs/pub.in > %s/program.out",dir,sFile,dir,dir);
+	system(cmd);
+	
+	sprintf(cmd,"vimdiff %s/program.out %s/pubs/pub.out",dir,dir);
+	system(cmd);
+
+	return 0;
+}
